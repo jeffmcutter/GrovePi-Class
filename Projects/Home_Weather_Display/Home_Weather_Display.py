918331dc940a0f1bab149121ac172e16272956de
Jeffrey Cutter <jeff_m_cutter@yahoo.com>
2018-02-24 23:18:26 -0500

    Add Extra Practice items for Module 2.

--- Projects/Home_Weather_Display/Home_Weather_Display.py
+++ Projects/Home_Weather_Display/Home_Weather_Display.py
@@ -42,6 +42,7 @@ from time import sleep
 from math import isnan
 import paho.mqtt.client as mqtt
 import json
+import grovepi
 
 # Define a class NanError derived from super class Exception for use in raising Nan errors.
 
@@ -51,13 +52,28 @@ class NanError(Exception):
 
 
 # Set to True to see debug statements like nan error, False to hide.
-debug = True
+debug = False
 
+# Setup DHT sensor.
 dht_sensor_port = 7     # connect the DHt sensor to port 7
 dht_sensor_type = 0     # use 0 for the blue-colored sensor and 1 for the white-colored sensor
 
-scale = 'F'
-symbol = u'\u2103'
+scale = 'F'         # Scale for temperature, C or F.
+symbol = u'\u2103'  # Unicode degrees C.
+
+
+# Setup Rotary Angle Sensor
+potentiometer = 0   # Connect the Grove Rotary Angle Sensor to analog port A0
+led = 8             # Connect the LED to digital port D8
+grovepi.pinMode(potentiometer, 'INPUT')
+grovepi.pinMode(led,"OUTPUT")
+sleep(1)            # Not sure it's needed, but leave for now.
+adc_ref = 5         # Reference voltage of ADC is 5v
+grove_vcc = 5       # Vcc of the grove interface is normally 5v
+full_angle = 300    # Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
+
+# Setup Ultrasonic Ranger
+ultrasonic_ranger = 3
 
 # Initialize MQTT connections.
 local_client = mqtt.Client()
@@ -69,6 +85,13 @@ remote_client.loop_start()
 
 try:
     while True:
+        # Read Ultrasonic Ranger
+        ranger = grovepi.ultrasonicRead(ultrasonic_ranger)
+
+        # Read sensor value from potentiometer
+        angle_sensor = grovepi.analogRead(potentiometer)
+
+        # DHT in it's own try except to handle nan errors.
         try:
             # get the temperature and Humidity from the DHT sensor
             [temp, hum] = dht(dht_sensor_port, dht_sensor_type)
@@ -78,43 +101,62 @@ try:
             if isnan(temp) is True or isnan(hum) is True:
                 raise NanError('nan error')
 
-            if temp <= 5:
-                setRGB(0, 0, 255)
-            elif 5 < temp < 20:
-                setRGB(0, 255, 0)
-            else:
-                setRGB(255, 0, 0)
-
-            print '\nTemp:', str(temp) + u'\u2103'
-
-            if scale == 'F':
-                temp = temp * 9 / 5 + 32
-                symbol = u'\u2109'
-                print 'Temp:', str(temp) + symbol
-
-            print 'Humi:', str(hum) + '%'
-
-            # instead of inserting a bunch of whitespace, we can just insert a \n
-            # we're ensuring that if we get some strange strings on one line, the 2nd one won't be affected
-            # setText_norefresh('Temp: ' str(temp) + scale + '\n' + 'Humidity: ' + str(hum) + '%')
-            setText_norefresh(str(temp) + scale + '\n' + str(hum) + '%')
-
-            # Publish to MQTT.
-            local_client.publish('SNHU/IT697/sensor/data', 'Temp: ' + str(temp) + scale + ', Humi: ' + str(hum) + '%')
-            publish_data = {"timestamp": int(time.time() * 1000), "data": {"temperature": temp, "humidity": hum}}
-            local_client.publish('SNHU/IT697/sensor/data/json', json.dumps(publish_data))
-            remote_client.publish('SNHU/IT697/jeffrey_cutter_snhu_edu/sensor/data/json', json.dumps(publish_data))
-
         except NanError as e:
             if debug:
                 print str(e)
             # Do nothing and retry after short delay.
-            sleep(0.2)
+            sleep(0.25)
             continue
 
+        if temp <= 5:
+            setRGB(0, 0, 255)
+        elif 5 < temp < 20:
+            setRGB(0, 255, 0)
         else:
-            # Delay between updates.
-            sleep(3)
+            setRGB(255, 0, 0)
+
+        print '\nTemp:', str(temp) + u'\u2103'  # Unicode degrees C.
+
+        if scale == 'F':
+            temp = temp * 9 / 5 + 32
+            symbol = u'\u2109'  # Unicode degrees F.
+            print 'Temp:', str(temp) + symbol
+
+        print 'Humi:', str(hum) + '%'
+
+        # Calculate voltage
+        voltage = round(float(angle_sensor) * adc_ref / 1023, 2)
+
+        # Calculate rotation in degrees (0 to 300)
+        degrees = round((voltage * full_angle) / grove_vcc, 2)
+
+        # Calculate LED brightess (0 to 255) from degrees (0 to 300)
+        brightness = int(degrees / full_angle * 255)
+
+        # Give PWM output to LED
+        grovepi.analogWrite(led, brightness)
+
+        print "Ranger:", ranger
+
+        print("Angle: %d, Voltage: %.2f, degrees: %.1f, brightness: %d" % (angle_sensor, voltage, degrees, brightness))
+
+        # instead of inserting a bunch of whitespace, we can just insert a \n
+        # we're ensuring that if we get some strange strings on one line, the 2nd one won't be affected
+        setText_norefresh("Temp: " + str(temp) + scale + '\n' + "Humidity: " + str(hum) + '%')
+
+        # Publish to MQTT.
+        local_client.publish('SNHU/IT697/sensor/data', 'Temp: ' + str(temp) + scale + ', Humi: ' + str(hum) + '%')
+        local_client.publish('SNHU/IT697/sensor/data', 'Ranger: ' + str(ranger))
+        local_client.publish('SNHU/IT697/sensor/data', 'Degrees: ' + str(degrees))
+        publish_data = {"timestamp": int(time.time() * 1000),
+                        "data": {"temperature": temp, "humidity": hum, "range": ranger, "degrees": degrees}}
+        local_client.publish('SNHU/IT697/sensor/data/json', json.dumps(publish_data))
+        remote_client.publish('SNHU/IT697/jeffrey_cutter_snhu_edu/sensor/data/json', json.dumps(publish_data))
+
+        # Delay between updates.
+        sleep(2)
+        if debug:
+            print
 
 except KeyboardInterrupt as e:
     if debug:
@@ -127,4 +169,5 @@ finally:
     setText('')
     # And turn off the display.
     setRGB(0, 0, 0)
-
+    print "Turning LED off"
+    grovepi.analogWrite(led,0)
