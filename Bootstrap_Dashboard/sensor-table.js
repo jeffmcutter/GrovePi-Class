231eb8466db158d13893bdb70d5774ce8cb026ef
Jeffrey Cutter <jeff_m_cutter@yahoo.com>
2018-03-31 17:48:30 -0400

    Add orginal sensor-table files.

---
+++ Bootstrap_Dashboard/sensor-table.js
@@ -0,0 +1,11 @@
+Template.sensorTable.helpers({
+    sensors: function() {
+        return Sensors.find();
+    }
+});
+
+Template.sensorRow.helpers({
+    datetime: function() {
+        return Date(Template.currentData().timestamp);
+    }
+});
