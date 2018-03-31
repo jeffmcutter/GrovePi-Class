073e2c3fd093fdaafb8163136f6d41cbf0909bf4
Jeffrey Cutter <jeff_m_cutter@yahoo.com>
2018-03-31 17:49:28 -0400

    Add modified sensor-table files.

--- Bootstrap_Dashboard/sensor-table.js
+++ Bootstrap_Dashboard/sensor-table.js
@@ -1,11 +1,23 @@
 Template.sensorTable.helpers({
     sensors: function() {
         return Sensors.find();
-    }
+    },
 });
 
 Template.sensorRow.helpers({
     datetime: function() {
         return Date(Template.currentData().timestamp);
     }
+});
+
+UI.registerHelper("arrayify", function(obj){
+    var result = [];
+    var keys = [];
+    for (var key in obj){
+        keys.push(key);
+    }
+    for (var key in keys.sort()){
+        result.push({name:keys[key],value:obj[keys[key]]});
+    }
+    return result;
 });
